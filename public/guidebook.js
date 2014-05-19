adfbd453428b1ace84fce4e3cacbfafe63cdee83
Damon Oehlman <damon.oehlman@gmail.com>
2014-05-19 22:11:48 +1000

    Starting to use browser-module-sandbox to run code

--- public/guidebook.js
+++ public/guidebook.js
@@ -1,2 +1,42 @@
 var qsa = require('fdom/qsa');
+var createSandbox = require('browser-module-sandbox');
+var crel = require('crel');
 
+function initCodeSection(el) {
+  var runButton = crel('button', 'run');
+
+  if (el.nextElementSibling) {
+    el.parentNode.insertBefore(el.nextElementSibling, runButton);
+  }
+  else {
+    el.parentNode.appendChild(runButton);
+  }
+
+  runButton.addEventListener('click', prepareClickHandler(el));
+}
+
+function prepareClickHandler(el) {
+  return function(evt) {
+    var sandbox = createSandbox({
+      cdn: 'http://localhost:3000',
+      container: document.querySelector('div.play'),
+      iframeStyle: 'body, html { height: 100%; width: 100%; }',
+      cacheOpts: {
+        inMemory: true
+      }
+    });
+
+    sandbox
+      .on('modules', function(modules) {
+      })
+      .on('bundleStart', function() {
+      })
+      .on('bundleEnd', function(html) {
+        console.log('bundling done: ', html);
+      });
+
+    sandbox.bundle(el.innerText);
+  };
+}
+
+qsa('div.highlight').forEach(initCodeSection);
